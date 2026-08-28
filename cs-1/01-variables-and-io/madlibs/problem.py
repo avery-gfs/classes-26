4d72119c5fc7a3d84aeb4aa41151758ebcde1d51
Avery <anortonsmith@germantownfriends.org>
2026-08-28 15:35:19 -0400

    update problems

---
+++ cs-1/01-variables-and-io/madlibs/problem.py
@@ -0,0 +1,22 @@
+# Template
+#
+# When buying a used car, remember: never <verb> the engine if the car is <adjective>.
+# Before driving, make sure the gas tank is free of <plural noun>.
+# Used cars perform best when driven between <number> and <number> miles per hour.
+# Always keep a large supply of <substance> in your car for passengers to enjoy.
+#
+# Inputs
+#
+# verb: cook
+# adjective: spicy
+# plural noun: frogs
+# number: 2
+# number: 200
+# substance: rice pudding
+#
+# Result
+#
+# When buying a used car, remember: never cook the engine if the car is spicy.
+# Before driving, make sure the gas tank is free of frogs.
+# Used cars perform best when driven between 2 and 200 miles per hour.
+# Always keep a large supply of rice pudding in your car for passengers to enjoy.
--- cs-1/01-variables-and-io/readme.md
+++ cs-1/01-variables-and-io/readme.md
@@ -290,12 +290,16 @@ Tate, B. A. (2010). Seven Languages in Seven Weeks. The Pragmatic Bookshelf
 
 ## Mad Libs
 
+Template
+
 ```
 When buying a used car, remember: never <verb> the engine if the car is <adjective>.
 Before driving, make sure the gas tank is free of <plural noun>.
 Used cars perform best when driven between <number> and <number> miles per hour.
 Always keep a large supply of <substance> in your car for passengers to enjoy.
 ```
+
+Inputs
 
 ```
 verb: cook
@@ -305,6 +309,8 @@ number: 2
 number: 200
 substance: rice pudding
 ```
+
+Result
 
 ```
 When buying a used car, remember: never cook the engine if the car is spicy.
