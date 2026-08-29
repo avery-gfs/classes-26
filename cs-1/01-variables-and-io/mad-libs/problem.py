d41f683f48066c6d6b459eba888f733dbdce8b32
Avery <anortonsmith@germantownfriends.org>
2026-08-29 14:42:49 -0400

    update problems

--- cs-1/01-variables-and-io/mad-libs/problem.py
+++ cs-1/01-variables-and-io/mad-libs/problem.py
@@ -1,6 +1,6 @@
 # Template
 #
-# When buying a used car, remember: never <verb> the engine if the car is <adjective>.
+# When operating a used car, remember: never <verb> the engine if the car is <adjective>.
 # Before driving, make sure the gas tank is free of <plural noun>.
 # Used cars perform best when driven between <number> and <number> miles per hour.
 # Always keep a large supply of <substance> in your car for passengers to enjoy.
@@ -16,7 +16,7 @@
 #
 # Result
 #
-# When buying a used car, remember: never cook the engine if the car is spicy.
+# When operating a used car, remember: never cook the engine if the car is spicy.
 # Before driving, make sure the gas tank is free of frogs.
 # Used cars perform best when driven between 2 and 200 miles per hour.
 # Always keep a large supply of rice pudding in your car for passengers to enjoy.
