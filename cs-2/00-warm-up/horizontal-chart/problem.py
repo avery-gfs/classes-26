b65d3f8a9cf56d95f3c56bd736a2e96abea6f470
Avery <anortonsmith@germantownfriends.org>
2026-08-31 11:11:19 -0400

    update problems

---
+++ cs-2/00-warm-up/horizontal-chart/problem.py
@@ -0,0 +1,15 @@
+# Given a list of integers each greater than zero, generate a horizontal bar
+# chart where each number `n` is shown as a row column of `X` characters of
+# length `n`.
+#
+# For the numbers [1, 4, 2, 7, 3, 1] the output should be:
+#
+# X
+# XXXX
+# XX
+# XXXXXXX
+# XXX
+# X
+
+
+numbers = [1, 4, 2, 7, 3, 1]
